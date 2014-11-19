c5cad17d4c4349edfd3a520b8e5f131e6bd43866
Itamar Nakar <user@MacBook-Pro.local>
2014-11-19 15:44:47 +0200

    Initial Commit

---
+++ Man's besr friend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Man's besr friend
+//
+//  Created by Itamar Nakar on 19/11/14.
+//  Copyright (c) 2014 Itamar Nakar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Man's besr friend/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Man's besr friend
+//
+//  Created by Itamar Nakar on 19/11/14.
+//  Copyright (c) 2014 Itamar Nakar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
